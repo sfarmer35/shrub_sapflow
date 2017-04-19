e03a6ab2e1aa79ea2c847211cc5892b5e3946bb4
sfarmer35 <sfarmer@colgate.edu>
2017-04-19 11:24:15 -0400

    added code for corrections

--- German_SA.R
+++ German_SA.R
@@ -4,6 +4,17 @@ setwd("C:\\Users\\Sabrina\\Google Drive\\Lab\\sapflow_sf\\German")
 #input variables data
 datSA<-read.csv("Area_inputvalues.csv")
 
+JDay<-datSA[3]
+JHM<-datSA[4]
+
+hourD<-JHM/100
+Hour3<-ifelse(floor(hourD$JHM)-hourD$JHM < 0, floor(hourD$JHM) + 0.5, floor(hourD$JHM))
+
+#changing DOY to start at 5am
+DOYSA<-ifelse(Hour3 < 5, JDay$JDAY - 1, JDay$JDAY)
+head(JDay)
+unique(Hour)
+
 C<-datSA[5:20]
 B<-datSA[21:36]
 A<-datSA[37:52]
@@ -12,6 +23,18 @@ dT<-datSA[69:84]
 SA<-datSA[85:100]
   #these values are now their own "data". What does it mean to be "data" vs "values"?
   #which do I want them to be?
+
+Qv<-matrix(rep(NA,dim(SA)[1]*16), ncol=16)
+Kshapp<-matrix(rep(NA,dim(SA)[1]*16), ncol=16)
+
+for(i in 1:16) {
+  Qv[,i]<-(0.42*SA[,i]*(B[,i]-A[,i]))/(4*10*0.04)
+  Kshapp[,i]<-(Pin[,i]-Qv[,i])/C[,i]
+  Kmin[[i]]<-aggregate(Kshapp[,i], by=list(DOYSA), FUN="min", na.action=na.omit)
+}
+
+
+
 
 ##### Heather Old Code #####
   #DG_Pin(i) = H_V(i) * H_V(i) / DG_HR(i)
