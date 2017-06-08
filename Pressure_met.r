29ad3c370a55f3d30cf5b45bd0a2857f70b38cd0
kroppheather <kroppheather@gmail.com>
2017-06-08 12:45:34 -0400

    added script for processing pressure data

---
+++ Pressure_met.r
@@ -0,0 +1,19 @@
+setwd("c:\\Users\\hkropp\\Google Drive\\sapflow_sf\\met")
+datR<-read.table("airport.csv.csv", sep=";", header=TRUE)
+
+#convert pressure from mm per HG to Kpa
+Pnew<-datR$Po*(1/7.501)
+
+#now convert dates
+library(lubridate)
+timeR<-gsub(".","/",)
+
+dateR<-as.Date(row.names(datR), "%d.%m.%Y %H:%M")
+doyR<-yday(dateR)
+YearR<-year(dateR)
+
+Pday<-aggregate(Pnew, by=list(doyR, YearR), FUN="mean", na.omit=TRUE)
+
+Pday$PdayGap<-ifelse(is.na(Pday$x),(Pday$x[-1]+Pday$x[+1])/2,Pday$x)
+colnames(Pday)[1:3]<-c("doy","year","Pkpa")
+write.table(Pday,"Pressure.csv", sep=",",row.names=FALSE)
